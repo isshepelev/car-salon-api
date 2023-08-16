b16e2c60f8695d1c74b9689d54fa6c50995bd7b8
isshepelev <isshepelev2004@gmail.com>
2023-08-16 15:41:40 +0300

    add validation annotations

--- src/main/java/ru/isshepelev/carsalonapi/entity/job/DTO/JobDTO.java
+++ src/main/java/ru/isshepelev/carsalonapi/entity/job/DTO/JobDTO.java
@@ -2,10 +2,19 @@ package ru.isshepelev.carsalonapi.entity.job.DTO;
 
 import lombok.Data;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 
 @Data
 public class JobDTO {
+    @NotBlank(message = "Title cannot be blank")
+    @Pattern(regexp = "^[a-zA-Zа-яА-Я0-9]{1,20}$", message = "Invalid input format title")
     private String title;
+
+    @NotBlank(message = "Payment cannot be blank")
+    @DecimalMin(value = "0.01", message = "Payment must be greater than or equal to 0.01")
+    @Pattern(regexp = "^(?!0\\.01)\\d+(\\.\\d{2})?$", message = "Invalid money format payment")
     private BigDecimal payment;
 }
--- src/main/java/ru/isshepelev/carsalonapi/entity/job/DTO/JobPaymentUpdateDTO.java
+++ src/main/java/ru/isshepelev/carsalonapi/entity/job/DTO/JobPaymentUpdateDTO.java
@@ -2,9 +2,15 @@ package ru.isshepelev.carsalonapi.entity.job.DTO;
 
 import lombok.Data;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 
 @Data
 public class JobPaymentUpdateDTO {
+    @NotBlank(message = "Payment cannot be blank")
+    @DecimalMin(value = "0.01", message = "Payment must be greater than or equal to 0.01")
+    @Pattern(regexp = "^(?!0\\.01)\\d+(\\.\\d{2})?$", message = "Invalid money format payment")
     private BigDecimal payment;
 }
